f0732f9122119640ea6bf63d1a34384dab194ddb
Arne Berner <info@arneberner.de>
2024-05-23 09:07:48 +0200

    bugfix: added a space so that its not xclipfish

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ set -e
 echo "Utilities"
 sudo add-apt-repository universe
 sudo apt install -y fish python3-pip \
-curl zathura tmux clang xclip\
+curl zathura tmux clang xclip \
 fzf git pkg-config libssl-dev libfuse2
 sudo apt-get install lld
 
