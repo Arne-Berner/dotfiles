6e7c511c4ebeb1275d38f548fe15c8d1b77cb001
Arne Berner <info@arneberner.de>
2026-01-04 01:27:08 +0100

    feat: added more default lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,14 +1,14 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
     end
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     config = function()
-      require("mason-lspconfig").setup({ ensure_installed={"pyright"}})
+      require("mason-lspconfig").setup({ ensure_installed={"pyright", "rust_analyzer", "wgsl_analyzer", "tinymist" }})
     end
   },
   {
