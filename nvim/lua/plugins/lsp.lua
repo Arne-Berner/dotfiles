7e89e78c91de550b5ef6c1402a0f3b7da2819e7e
Arne Berner <info@arneberner.de>
2026-01-04 01:27:30 +0100

    fix: changed hotkeys to old and used new api for lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,24 +4,30 @@ return	{
 		'neovim/nvim-lspconfig',
 		config = function()
 			-- Setup language servers.
-			local lspconfig = require('lspconfig')
+			local lspconfig = vim.lsp.config
 
       --python
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-      lspconfig.pyright.setup{
-        capabilities=capabilities,
-      }
+      vim.lsp.enable("pyright")
 
       -- WGSL 
-      -- lspconfig.wgsl_analyzer.setup {}
-      lspconfig.glasgow.setup {}
+      vim.lsp.enable('wgsl_analyzer')
+      lspconfig.wgsl_analyzer.filetypes = {"wgsl", "wesl"}
       
 			-- Rust
-			lspconfig.rust_analyzer.setup {
+      vim.lsp.enable("rust_analyzer")
+			lspconfig.rust_analyzer = {
 				-- Server-specific settings. See `:help lspconfig-setup`
 				settings = {
 					["rust-analyzer"] = {
+            procMacro = {
+                    ignored = {
+                        leptos_macro = {
+                            -- optional: --
+                            -- "component",
+                            "server",
+                        },
+                    },
+                  },
 						cargo = {
 							allFeatures = true,
 						},
@@ -48,53 +54,17 @@ return	{
 					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
 					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-          -- might want to use <C-g> instead of space?
+					-- See `:help vim.lsp.buf` and vim.diagnostic for documentation on any of the below functions
+          -- the normal map for them is 
 					local opts = { buffer = ev.buf }
-					vim.keymap.set('n', '<space>D', vim.lsp.buf.declaration, opts)
-					vim.keymap.set('n', '<space>d', vim.lsp.buf.definition, opts)
-					vim.keymap.set('n', '<space><space>', vim.lsp.buf.hover, opts)
-					vim.keymap.set('n', '<space>i', vim.lsp.buf.implementation, opts)
-          -- will show diagnostics
-					vim.keymap.set('n', '<space>n', vim.diagnostic.goto_next, opts)
-					vim.keymap.set('n', '<space>p', vim.diagnostic.goto_prev, opts)
-					--vim.keymap.set('n', '<space>n', vim.lsp.buf.add_workspace_folder, opts)
-					-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-					-- vim.keymap.set('n', '<leader>wl', function()
-						-- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					-- end, opts)
-					--vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts).bak
+					vim.keymap.set('n', 'grd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'grD', vim.lsp.buf.declaration, opts)
 					vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
 					vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, opts)
 					vim.keymap.set('n', '<F3>', function()
 						vim.lsp.buf.format { async = true }
 					end, opts)
 
-
-          local function refs()
-            vim.lsp.buf.references()
-            vim.cmd([[
-                augroup CloseQuickfixOnLeave
-                autocmd!
-                autocmd WinLeave * if &filetype == 'qf' | q |
-                autocmd FileType qf nnoremap <buffer> <esc> :q<cr>
-                augroup END
-            ]])
-          end
-
-					vim.keymap.set('n', '<space>r', refs, opts)
-					local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
-					-- When https://neovim.io/doc/user/lsp.html#lsp-inlay_hint stabilizes
-					-- *and* there's some way to make it only apply to the current line.
-					-- if client.server_capabilities.inlayHintProvider then
-					--     vim.lsp.inlay_hint(ev.buf, true)
-					-- end
-
-					-- None of this semantics tokens business.
-					-- https://www.reddit.com/r/neovim/comments/143efmd/is_it_possible_to_disable_treesitter_completely/
-          -- TODO maybe use this for no treesitter?
-					--client.server_capabilities.semanticTokensProvider = nil
 				end,
 			})
 		end
@@ -116,18 +86,10 @@ return	{
 	},
 
 
-  -- typst
-  {
-      require'lspconfig'.typst_lsp.setup{
-      settings = {
-        exportPdf = "onType" -- Choose onType, onSave or never.
-            -- serverPath = "" -- Normally, there is no need to uncomment it.
-      }
-    }
-  },
 	-- language support
 	-- toml
 	'cespare/vim-toml',
+
 	-- yaml
 	{
 		"cuducos/yaml.nvim",
@@ -136,6 +98,7 @@ return	{
 			"nvim-treesitter/nvim-treesitter",
 		},
 	},
+
 	-- rust
 	{
 		'rust-lang/rust.vim',
